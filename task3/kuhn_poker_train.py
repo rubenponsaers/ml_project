37b72f67daad4524ece4842d5f8d7888e1fb4be6
rubenponsaers <rubenponsaers@gmail.com>
2022-05-15 11:43:20 +0200

    task 3 update

--- task3/kuhn_poker_train.py
+++ task3/kuhn_poker_train.py
@@ -2,43 +2,69 @@ from absl import app
 
 import pyspiel
 import tensorflow.compat.v1 as tf
-import my_project.eval_tools as eval_tools
+import my_project.exploit_nash as exploit_nash
+import my_project.extract_policy as extract_policy
+import my_project.evaluation_opponents as evaluation_opponents
 
 from open_spiel.python import rl_environment
 from open_spiel.python.algorithms import tabular_qlearner
-from open_spiel.python.algorithms import random_agent
 
 
 def train_kuhn(env, alg, agents):
 	num_actions = env.action_spec()["num_actions"]
 	game = pyspiel.load_game("kuhn_poker")
-	num_episodes = 15000 
+	num_episodes = 1000 # number of iterations
 	num_eval = 100	# how many evaluations are done against a specific agent
-	step_size = 1000 # after how many steps does an evaluation happen
+	step_size = 100 # after how many steps does an evaluation happen
 	
 	# evaluation structures
-	exploitability_evolution = [[] for _ in range(len(agents))]
+	exploitability_evolution = [[],[]]
 	nashconv_evolution = [[]]
-	utility_evolution_against_random = [[] for _ in range(len(agents))]
+	utility_against_random_evolution = [[],[]]
 	
+	# Training information
+	print("---------- "+alg+" ----------")
+	print("Number of actions: "+str(num_actions))
+	print("Game: "+str(game))
+	print("Number of episodes: "+str(num_episodes))
+	print("Number of evaluations: "+str(num_eval))
+	print("---------- "+alg+" (START) ----------")
+
 	# start training agents
-	pol = train_agents(env, alg, agents, num_actions, num_episodes, step_size, num_eval,
-		exploitability_evolution, nashconv_evolution, utility_evolution_against_random)
-	
+	pol = train_agents(env, game, alg, agents, num_actions, num_episodes, step_size, num_eval,
+		exploitability_evolution, nashconv_evolution, utility_against_random_evolution)
+
 	# save evaluation structures
+
 
 	# save final policy
 
 	# draw figures
 
 # Train agents based on a specific algorithm
-def train_agents(env, alg, agents, num_actions, num_episodes, step_size, num_eval, 
+def train_agents(env, game, alg, agents, num_actions, num_episodes, step_size, num_eval, 
 	exploitability_evolution, nashconv_evolution, utility_evolution_against_random):
 	
 	# Start training loop
 	for i in range(num_episodes):
+		
+		# Evaluate current agents
 		if i % step_size == 0:
 			print("Training progress: "+str(i)+"/"+str(num_episodes))
+			# Get current policy
+			pol = extract_policy.ExtractPolicy(env,agents)
+
+			# Exploitability
+			# expl = exploit_nash.exploitability(game, pol)
+
+			# NashConv
+			# nash = exploit_nash.nash_conv(game,pol)
+
+			# Evaluation against random
+			(avg_u, avg_w) = evaluation_opponents.evaluate_against_random(env,agents[0],0,alg,num_actions,num_eval)
+			print((avg_u, avg_w))
+	
+		# Play a game
 		time_step = env.reset()
 		while not time_step.last():
 			player_id = time_step.observations["current_player"]
@@ -46,12 +72,9 @@ def train_agents(env, alg, agents, num_actions, num_episodes, step_size, num_eva
 			time_step = env.step([agent_output.action])
 		for agent in agents:
 			agent.step(time_step)
-		
-		# Get current policy with available 
 
-
-	print("Training agents of " + alg + " complete.")
-	return
+	print("---------- "+alg+" (COMPLETE) ----------")
+	return pol
 
 
 # Get agents for Q-learning
@@ -60,7 +83,7 @@ def get_qlearning_agents(env,sess):
 	agent1 = tabular_qlearner.QLearner(player_id=0, num_actions=num_actions)
 	agent2 = tabular_qlearner.QLearner(player_id=1, num_actions=num_actions)
 	return [agent1, agent2]
-
+	
 def main(_):
 	env = rl_environment.Environment("kuhn_poker")
 	sess = tf.Session()
