d0f3ed9be5cf06465f936501e1be4cbe4547323d
sennecolson <sennecolson18@gmail.com>
2022-05-16 13:51:12 +0200

    v1 of DQN

--- code/part2_utils.py
+++ code/part2_utils.py
@@ -1,19 +1,43 @@
+from cProfile import label
 import pyspiel
 
 from open_spiel.python import rl_environment
-from open_spiel.python import rl_tools
-from open_spiel.python.algorithms import tabular_qlearner, action_value
-from open_spiel.python.egt import dynamics, visualization, utils
+from open_spiel.python.algorithms import tabular_qlearner, action_value, random_agent
+from open_spiel.python.egt import dynamics, utils
 
 import matplotlib.pyplot as plt
-#from matplotlib import Figure
-#from matplotlib.figure import Figure
 from matplotlib.quiver import Quiver
 from matplotlib.streamplot import StreamplotSet
 
 import dynamics_self
 
 import numpy as np
+
+def _eval_against_random_bots(env, trained_agents, random_agents, num_episodes):
+  """Evaluates `trained_agents` against `random_agents` for `num_episodes`."""
+  num_players = len(trained_agents)
+  sum_episode_rewards = np.zeros(num_players)
+  for player_pos in range(num_players):
+    cur_agents = random_agents[:]
+    cur_agents[player_pos] = trained_agents[player_pos]
+    for _ in range(num_episodes):
+      time_step = env.reset()
+      episode_rewards = 0
+      while not time_step.last():
+        player_id = time_step.observations["current_player"]
+        if env.is_turn_based:
+          agent_output = cur_agents[player_id].step(
+              time_step, is_evaluation=True)
+          action_list = [agent_output.action]
+        else:
+          agents_output = [
+              agent.step(time_step, is_evaluation=True) for agent in cur_agents
+          ]
+          action_list = [agent_output.action for agent_output in agents_output]
+        time_step = env.step(action_list)
+        episode_rewards += time_step.rewards[player_pos]
+      sum_episode_rewards[player_pos] += episode_rewards
+  return sum_episode_rewards / num_episodes
 
 def train(game):
     #initialization of the environment 
@@ -22,13 +46,15 @@ def train(game):
     num_actions = env.action_spec()["num_actions"]
 
     #initialization of the agents
-    agent1 = tabular_qlearner.QLearner(player_id=0, num_actions=num_actions)
-    agent2 = action_value.TreeWalkCalculator(game)
-    agents = [agent1, agent2]
-    #agents = [
-    #    tabular_qlearner.QLearner(player_id=idx, num_actions=num_actions)
-    #    for idx in range(num_players)
-    #]
+    agents = [
+        tabular_qlearner.QLearner(player_id=idx, num_actions=num_actions)
+        for idx in range(num_players)
+    ]
+
+    random_agents = [
+        random_agent.RandomAgent(player_id=idx, num_actions=num_actions)
+        for idx in range(num_players)
+    ]
 
     agent1_losses_avg = []
     agent2_losses_avg = []
@@ -36,16 +62,30 @@ def train(game):
     agent1_losses = []
     agent2_losses = []
 
-    #training of the agents via Q-learning algorithm (and self-play)
-    for cur_episode in range(25000):
-        if cur_episode % 1000 == 0:
-            print(f"Episodes: {cur_episode}")
-            if cur_episode != 0:
-                agent1_losses_avg.append(sum(agent1_losses)/len(agent1_losses))
-                agent1_losses.clear()
+    agent1_probs = []
+    agent2_probs = []
 
-                agent2_losses_avg.append(sum(agent2_losses)/len(agent2_losses))
-                agent2_losses.clear()
+    agent1_probs_avg = []
+    agent2_probs_avg = []
+
+    #training of the agents via Q-learning algorithm (and self-play)
+    for cur_episode in range(100):
+        if (cur_episode+1) % 10 == 0:
+            print("Episodes: {}".format(cur_episode+1))
+            agent1_losses_avg.append(sum(agent1_losses)/len(agent1_losses))
+            agent1_losses.clear()
+
+            agent2_losses_avg.append(sum(agent2_losses)/len(agent2_losses))
+            agent2_losses.clear()
+
+            agent1_probs_avg.append(sum(agent1_probs)/len(agent1_probs))
+            agent1_probs.clear()
+
+            agent2_probs_avg.append(sum(agent2_probs)/len(agent2_probs))
+            agent2_probs.clear()
+
+            #print(_eval_against_random_bots(env, agents, random_agents, 1000))
+
         time_step = env.reset()
         while not time_step.last():
             agent1_output = agents[0].step(time_step)
@@ -54,11 +94,19 @@ def train(game):
         # Episode is over, step all agents with final info state.
         for agent in agents:
             agent.step(time_step)
+
         
+        agent1_probs.append(agent1_output.probs)
+        agent2_probs.append(agent2_output.probs)
+
         agent1_losses.append(agents[0].loss)
         agent2_losses.append(agents[1].loss)
 
+
     print("Done!")
+    plt.plot(agent1_probs_avg)
+    plt.legend(['Rock', 'Paper', 'Scissors'])
+    plt.show()
 
     return agents[0], agents[1]
 
--- kuhn/my_project/models.py
+++ kuhn/my_project/models.py
@@ -4,17 +4,15 @@ from __future__ import print_function
 
 import yaml
 
-from open_spiel.python import policy
-from open_spiel.python.algorithms import deep_cfr_tf2
-from open_spiel.python.algorithms import exploitability
+from open_spiel.python import policy, rl_environment
+from open_spiel.python.algorithms import deep_cfr_tf2, exploitability, dqn, random_agent
+import tensorflow.compat.v1 as tf
 
 import pyspiel
 
 
 import pandas as pd
-
-
-import tensorflow as tf
+import numpy as np
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -86,9 +84,90 @@ def deep_cfr_model_tf2():
             print('Deep CFR in Kuhn Poker 2p. NashConv: {} after {} steps'.format(conv, step))
     return deep_cfr_solver
 
+def dqn_model():
+    config = load_config('dqn')
+    game = pyspiel.load_game(config['game'])
+    print(game)
+    print("Training a DQN model with following configuration: {}".format(config))
+    env = rl_environment.Environment(game)
+    info_state_size = env.observation_spec()["info_state"][0]
+    num_actions = env.action_spec()["num_actions"]
+
+    # random agents for evaluation
+    random_agents = [
+        random_agent.RandomAgent(player_id=idx, num_actions=num_actions)
+        for idx in range(config["num_players"])
+    ]
+
+    with tf.Session() as sess:
+        hidden_layers_sizes = [int(l) for l in config["hidden_layers_sizes"]]
+        # pylint: disable=g-complex-comprehension
+        agents = [
+            dqn.DQN(
+                session=sess,
+                player_id=idx,
+                state_representation_size=info_state_size,
+                num_actions=num_actions,
+                hidden_layers_sizes=hidden_layers_sizes,
+                replay_buffer_capacity=config["replay_buffer_capacity"],
+                learning_rate=config["learning_rate"],
+                epsilon_start=config["epsilon_start"],
+                epsilon_end=config["epsilon_end"],
+                batch_size=config["batch_size"]) for idx in range(config["num_players"])
+        ]
+        sess.run(tf.global_variables_initializer())
+        for step in range(config["num_steps"]):
+            if (step + 1) % config["eval_every"] == 0:
+                r_mean = _eval_against_random_bots(env, agents, random_agents, 100)
+                print("[{}] Mean episode rewards {}".format(step + 1, r_mean))
+            if (step + 1) % config["save_every"] == 0:
+                for agent in agents:
+                    agent.save(config["checkpoint_dir"])
+
+            time_step = env.reset()
+            while not time_step.last():
+                player_id = time_step.observations["current_player"]
+            if env.is_turn_based:
+                agent_output = agents[player_id].step(time_step)
+                action_list = [agent_output.action]
+            else:
+                agents_output = [agent.step(time_step) for agent in agents]
+                action_list = [agent_output.action for agent_output in agents_output]
+            time_step = env.step(action_list)
+
+            # Episode is over, step all agents with final info state.
+            for agent in agents:
+                agent.step(time_step)
+
 
 def _policy_to_csv(game, policy, filename):
     df = pd.DataFrame(
             data=policy.action_probability_array,
             index=[s.history_str() for s in policy.states])
     df.to_csv(filename)
+
+def _eval_against_random_bots(env, trained_agents, random_agents, num_episodes):
+  """Evaluates `trained_agents` against `random_agents` for `num_episodes`."""
+  num_players = len(trained_agents)
+  sum_episode_rewards = np.zeros(num_players)
+  for player_pos in range(num_players):
+    cur_agents = random_agents[:]
+    cur_agents[player_pos] = trained_agents[player_pos]
+    for _ in range(num_episodes):
+      time_step = env.reset()
+      episode_rewards = 0
+      while not time_step.last():
+        player_id = time_step.observations["current_player"]
+        if env.is_turn_based:
+          agent_output = cur_agents[player_id].step(
+              time_step, is_evaluation=True)
+          action_list = [agent_output.action]
+        else:
+          agents_output = [
+              agent.step(time_step, is_evaluation=True) for agent in cur_agents
+          ]
+          action_list = [agent_output.action for agent_output in agents_output]
+        time_step = env.step(action_list)
+        episode_rewards += time_step.rewards[player_pos]
+      sum_episode_rewards[player_pos] += episode_rewards
+  return sum_episode_rewards / num_episodes
